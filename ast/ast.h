20eb096ae516987c2c258d8f1c77a95d877efeb8
Joshua Pepple <pepplejoshuatams@gmail.com>
2025-10-09 22:43:18 -0600

    Add AST node definitions and enums
    Introduces ast/ast.h with core AST node structure, enums for node kinds, binary/unary operators, and supporting types for source location and function parameters.

---
+++ ast/ast.h
@@ -0,0 +1,93 @@
+#ifndef AST_H
+#define AST_H
+
+#include <stddef.h>  // For size_t
+
+// Forward declarations
+typedef struct AstNode AstNode;
+typedef struct Type Type;
+
+// Source location for errors
+typedef struct {
+    const char *file;
+    int line;
+    int column;
+} Location;
+
+// Tagged union for AST nodes
+typedef enum {
+    AST_DECL_FUNCTION,
+    AST_DECL_VARIABLE,
+    AST_DECL_CONSTANT,
+    AST_DECL_TYPE,
+
+    AST_STMT_RETURN,
+    AST_STMT_IF,
+    AST_STMT_WHILE,
+    AST_STMT_BLOCK,
+    AST_STMT_EXPR,
+    AST_STMT_ASSIGN,
+
+    AST_EXPR_LITERAL_INT,
+    AST_EXPR_LITERAL_FLOAT,
+    AST_EXPR_LITERAL_STRING,
+    AST_EXPR_LITERAL_BOOL,
+    AST_EXPR_IDENTIFIER,
+    AST_EXPR_BINARY_OP,
+    AST_EXPR_UNARY_OP,
+    AST_EXPR_CALL,
+    AST_EXPR_INDEX,
+    AST_EXPR_MEMBER,
+    // Add more as needed
+} AstKind;
+
+// Binary/unary ops
+typedef enum {
+    BINOP_ADD, BINOP_SUB, BINOP_MUL, BINOP_DIV,
+    BINOP_EQ, BINOP_NE, BINOP_LT, BINOP_LE, BINOP_GT, BINOP_GE,
+    BINOP_AND, BINOP_OR
+} BinaryOp;
+
+typedef enum {
+    UNOP_NEG, UNOP_NOT
+} UnaryOp;
+
+typedef struct {
+  char *name;
+  AstNode *type;
+} FuncParam;
+
+// The AST node struct
+struct AstNode {
+    AstKind kind;
+    Location loc;
+    union {
+        // Declarations
+        struct { char *name; AstNode *type_expr; AstNode *body; } func_decl;
+        struct { char *name; AstNode *type_expr; AstNode *init; } var_decl;
+        struct { char *name; AstNode *type_expr; AstNode *value; } const_decl;
+        struct { char *name; AstNode *type_expr; } type_decl;
+
+        // Statements
+        struct { AstNode *expr; } return_stmt;
+        struct { AstNode *cond; AstNode *then_branch; AstNode *else_branch; } if_stmt;
+        struct { AstNode *cond; AstNode *body; } while_stmt;
+        struct { AstNode **stmts; size_t stmt_count; } block_stmt;
+        struct { AstNode *expr; } expr_stmt;
+        struct { AstNode *lhs; AstNode *rhs; } assign_stmt;
+
+        // Expressions
+        struct { long long value; } int_lit;
+        struct { double value; } float_lit;
+        struct { char *value; } str_lit;
+        struct { bool value; } bool_lit;
+        struct { char *name; } ident;
+        struct { BinaryOp op; AstNode *left; AstNode *right; } binop;
+        struct { UnaryOp op; AstNode *operand; } unop;
+        struct { AstNode *func; AstNode **args; size_t arg_count; } call;
+        struct { AstNode *array; AstNode *index; } index_expr;
+        struct { AstNode *object; char *member; } member_expr;
+    } data;
+};
+
+#endif
