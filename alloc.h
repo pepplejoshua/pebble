3daa3f3d078c80dd4da9517f495aa69a1f0b2a94
Joshua Pepple <pepplejoshuatams@gmail.com>
2025-10-10 01:02:55 -0600

    Remove StackAlloc stack allocator implementation
    Deleted the StackAlloc stack allocator code from alloc.c and alloc.h, including related structs and functions. This simplifies the allocation module by removing unused or deprecated stack allocation logic.

--- alloc.h
+++ alloc.h
@@ -14,24 +14,4 @@ void arena_init(Arena *arena, size_t initial_capacity);
 void *arena_alloc(Arena *arena, size_t size);
 void arena_free(Arena *arena);  // Frees the whole arena
 
-// Ephemeral stack allocator
-typedef struct StackScope {
-    char *buffer;
-    size_t capacity;
-    size_t used;
-    struct StackScope *prev;  // Stack link
-} StackScope;
-
-typedef struct {
-    StackScope *current;
-    Arena arena;  // Use the long-lived arena to allocate scope buffers
-    size_t scope_capacity;
-} StackAlloc;
-
-void stack_alloc_init(StackAlloc *sa, size_t arena_capacity, size_t scope_capacity);
-void stack_push_scope(StackAlloc *sa);
-void stack_pop_scope(StackAlloc *sa);
-void *stack_alloc(StackAlloc *sa, size_t size);
-void stack_alloc_free(StackAlloc *sa);
-
 #endif
