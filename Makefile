651c4550e82665348a24defcd0e8cd15a24f5bf2
Joshua Pepple <pepplejoshuatams@gmail.com>
2025-10-10 15:05:27 -0600

    Add lexer tests and CLI option to run them
    Introduces a test_lexer() function in main.c to run lexer tests on sample source code snippets. Updates Makefile to include lexer.o in build, adds '--test-lexer' CLI option, and updates usage instructions accordingly.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -g -I src
 # OBJS = alloc.o ast.o lexer.o parser.o symbol.o type.o checker.o main.o
-OBJS = src/alloc.o src/ast.o src/symbol.o src/type.o src/main.o
+OBJS = src/alloc.o src/ast.o src/lexer.o src/symbol.o src/type.o src/main.o
 
 peb: $(OBJS)
 	$(CC) $(CFLAGS) -o peb $(OBJS)
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "alloc.h"
 #include "symbol.h"
 #include "type.h"
+#include "lexer.h"
 #include <stdio.h>
 
 // Global allocators
@@ -28,6 +29,75 @@ void run_tests(void) {
     printf("Arena usage: %zu / %zu bytes\n", long_lived.used, long_lived.capacity);
 }
 
+void test_lexer(void) {
+    printf("=== Lexer Tests ===\n");
+
+    // Test cases
+    const char *test_sources[] = {
+        // Simple function
+        "fn add(a int, b int) int {\n    return a + b;\n}",
+
+        // Expression function
+        "fn square(x int) int => x * x",
+
+        // Variables and literals
+        "let name = \"hello\";\nvar count = 42;\nlet pi = 3.14;",
+
+        // Keywords and operators
+        "if true && false || !x >= 10 { return; }",
+
+        // Comments
+        "// This is a comment\nlet x = 5; // Another comment"
+    };
+
+    for (int i = 0; i < 5; i++) {
+        printf("\n--- Test %d ---\n", i + 1);
+        printf("Source: %s\n\n", test_sources[i]);
+
+        Lexer lexer;
+        lexer_init(&lexer, test_sources[i], "<test>");
+
+        Token token;
+        do {
+            token = lexer_next_token(&lexer);
+
+            printf("%-15s", token_type_name(token.type));
+            printf("'%s'", token.lexeme);
+            printf(" at %d:%d", token.location.line, token.location.column);
+
+            // Print parsed values for literals
+            switch (token.type) {
+                case TOKEN_INT:
+                    printf(" (value: %lld)", token.value.int_val);
+                    break;
+                case TOKEN_FLOAT:
+                    printf(" (value: %g)", token.value.float_val);
+                    break;
+                case TOKEN_STRING:
+                    printf(" (value: \"%s\")", token.value.str_val);
+                    break;
+                case TOKEN_TRUE:
+                case TOKEN_FALSE:
+                    printf(" (value: %s)", token.value.bool_val ? "true" : "false");
+                    break;
+                default:
+                    break;
+            }
+
+            printf("\n");
+
+            if (token.type == TOKEN_ERROR) {
+                printf("ERROR: %s\n", token.lexeme);
+                break;
+            }
+
+        } while (token.type != TOKEN_EOF);
+    }
+
+    printf("\n✓ Lexer tests completed\n");
+}
+
+
 int main(int argc, char **argv) {
     // Initialize systems
     arena_init(&long_lived, 1024 * 1024);
@@ -37,6 +107,8 @@ int main(int argc, char **argv) {
     if (argc > 1) {
         if (strcmp(argv[1], "--test") == 0) {
             run_tests();
+        } else if (strcmp(argv[1], "--test-lexer") == 0) {
+            test_lexer();
         } else {
             printf("Compiling: %s\n", argv[1]);
             // TODO: Add lexer/parser/passes here
@@ -44,6 +116,7 @@ int main(int argc, char **argv) {
     } else {
         printf("Usage: %s <source_file>\n", argv[0]);
         printf("       %s --test\n", argv[0]);
+        printf("       %s --test-lexer\n", argv[0]);  // Add this line
     }
 
     // Cleanup
