c957ceb8bd56444d9b5a70ecb70f65cfe34d24ed
Caleb-o <caleb.ottohayes@gmail.com>
2025-10-23 03:36:17 +1000

    Allow trailing comma in struct declaration Allow multiple fields to a single type in structs

--- src/parser.c
+++ src/parser.c
@@ -1386,6 +1386,11 @@ AstNode *parse_type_expression(Parser *parser) {
     // Allow empty struct
     if (!parser_check(parser, TOKEN_RBRACE)) {
       do {
+        // Allow trailing comma
+        if (parser_check(parser, TOKEN_RBRACE)) {
+          break;
+        }
+
         // Grow arrays if needed
         if (count >= capacity) {
           capacity *= 2;
@@ -1399,13 +1404,56 @@ AstNode *parse_type_expression(Parser *parser) {
           field_types = new_types;
         }
 
-        // Parse field: IDENTIFIER TYPE
+        // Parse field: IDENTIFIER (, IDENTIFIER ...) TYPE
         if (!parser_check(parser, TOKEN_IDENTIFIER)) {
           parser_error(parser, "Expected field name");
           return NULL;
         }
         parser_advance(parser);
         Token field_name = parser->previous;
+
+        if (parser_check(parser, TOKEN_COMMA)) {
+          size_t current_count = count;
+
+          field_names[count++] = str_dup(field_name.lexeme);
+
+          while(parser_match(parser, TOKEN_COMMA)) {
+            if (!parser_check(parser, TOKEN_IDENTIFIER)) {
+              parser_error(parser, "Expected field name");
+              return NULL;
+            }
+            parser_advance(parser);
+            Token field_name = parser->previous;
+
+            // Grow arrays if needed
+            if (count >= capacity) {
+              capacity *= 2;
+              char **new_names =
+                  arena_alloc(&long_lived, capacity * sizeof(char *));
+              AstNode **new_types =
+                  arena_alloc(&long_lived, capacity * sizeof(AstNode *));
+              memcpy(new_names, field_names, count * sizeof(char *));
+              memcpy(new_types, field_types, count * sizeof(AstNode *));
+              field_names = new_names;
+              field_types = new_types;
+            }
+
+            field_names[count++] = str_dup(field_name.lexeme);
+          }
+
+          field_types[current_count] = parse_type_expression(parser);
+          if (!field_types[current_count]) {
+            return NULL;
+          }
+
+
+          // Copy type to all fields
+          for (size_t i = current_count + 1; i < count; i++) {
+            field_types[i] = field_types[current_count];
+          }
+
+          continue;
+        }
 
         field_names[count] = str_dup(field_name.lexeme);
         field_types[count] = parse_type_expression(parser);
