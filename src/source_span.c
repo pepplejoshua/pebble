db3412742efa902b734595b87a9410e4b4ea4593
Joshua Pepple <pepplejoshuatams@gmail.com>
2026-04-25 01:37:17 -0600

    Cleanup

--- src/source_span.c
+++
@@ -1,7 +0,0 @@
-#include "source_span.h"
-
-/*
- * This compilation unit exists for build integration.
- * SourceSpan functionality is currently implemented as inline
- * helpers in source_span.h.
- */
